1994418bc2c78d89757b5dee334a4cccbafdc3b4
TvShow <yasser2010_eg@yahoo.com>
2024-05-29 16:06:18 +0300

    fist commit

---
+++ app/src/main/java/com/androiddevs/mvvmnewsapp/ui/fragment/BreackingNewsFragment.kt
@@ -0,0 +1,6 @@
+package com.androiddevs.mvvmnewsapp.ui.fragment
+
+import androidx.fragment.app.Fragment
+import com.androiddevs.mvvmnewsapp.R
+
+class BreackingNewsFragment :Fragment(R.layout.fragment_breaking_news)
---
+++ app/src/main/java/com/androiddevs/mvvmnewsapp/util/Constants.kt
@@ -0,0 +1,8 @@
+package com.androiddevs.mvvmnewsapp.util
+
+class Constants {
+    companion object{
+        const val API_KEY="1b3357acded94a5b94231b5e6a9018cb"
+        const val BASE_URL="https://newsapi.org/"
+    }
+}
